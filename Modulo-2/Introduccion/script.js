a2c8b1a5fe9b340eae9f889f3c799ecf36bb0c31
IsanorLopez <isanoralejandro@gmail.com>
2022-07-04 20:44:17 -0600

    Curso_ProfessionalCoding[ - Introduccion a Js ]

---
+++ Modulo-2/Introduccion/script.js
@@ -0,0 +1,8 @@
+console.log('Hola desde script js')
+document.write('Desde js a HTML')
+
+let nombreUsuario = prompt('Ingresa un nombre')
+let edadUsuario = prompt('Ingresa una edad')
+let hobbyUsuario = prompt('Ingresa un hobbie')
+
+console.log('bienvenido',nombreUsuario, 'Que tiene', edadUsuario, 'Su hobbie es', hobbyUsuario)
